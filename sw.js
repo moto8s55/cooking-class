8e4956e7edc1a7d018b5aca0e238464fd656d021
Hajime Sudou <moto8s55@gmail.com>
2026-06-06 11:56:18 +0900

    SW cache version update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taiwan-cc-v1';
+const CACHE = 'taiwan-cc-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192x192.png', '/icons/icon-512x512.png'];
 
 self.addEventListener('install', e => {
